be6f6024b957929c655745fda27f98c1ba9b5031
LucasDev-end <lucas.marcolino@sptech.school>
2024-11-22 17:28:52 -0300

    criação da tela home e tela de login

---
+++ BD/tabelas.sql
@@ -0,0 +1,63 @@
+create database culinaria;
+
+use culinaria;
+
+create table categoria(
+idCategoria int primary key auto_increment,
+nome varchar(45)
+);
+
+create table cadastro(
+idcadastro int primary key auto_increment,
+nome varchar(45),
+email varchar(45),
+cidade varchar(45),
+estado char(2),
+senha varchar(45),
+fkCategoria int,
+constraint fkCategoriaCadastro foreign key (fkcategoria)
+							references categoria(idCategoria)
+);
+
+create table receitas(
+idReceitas int primary key auto_increment,
+nome varchar(45),
+tempoPreparo varchar(20),
+fkCategoria int,
+constraint fkCategoriaReceitas foreign key (fkcategoria)
+							references categoria(idCategoria)
+);
+
+create table ingredientes(
+idIngredientes int primary key auto_increment,
+nome varchar(45)
+);
+
+create table item(
+fkReceita int,
+fkIngrediente int,
+qtdIngrediente varchar(45)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
